1010ef3fd3352f64bbeb25de2202d0303a4cab10
hstiv <satmak335@gmail.com>
2019-12-03 18:56:45 +0300

    d04/ex00

--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -1,7 +1,5 @@
 <?php
 	session_start();
-	$login = "";
-	$pswd = "";
 	if ($_GET["submit"] && $_GET["submit"] == "OK")
 	{
 		if ($_GET["login"])
@@ -9,17 +7,13 @@
 		if ($_GET["passwd"])
 			$_SESSION["passwd"] = $_GET["passwd"];
 	}
-	if ($_SESSION["login"])
-		$login = $_SESSION["login"];
-	if ($_SESSION["passwd"])
-		$pswd = $_SESSION["passwd"];
 ?>
 
 <html><body>
 	<form action="" method="GET">
-		Username: <input type="text" name="login" value="<?echo"$login"?>" />
+	Username: <input type="text" name="login" value= "<?php if ($_SESSION["login"]) {echo $_SESSION["login"];}?>" />
 		<br />
-		Password: <input type="text" name="passwd" value="<?echo "$pswd"?>" />
+	Password: <input type="text" name="passwd" value= "<?php if ($_SESSION["passwd"]){echo $_SESSION["passwd"];}?>" />
 		<input type="submit" name="submit" value="OK" />
 	</form>
 </body></html>
